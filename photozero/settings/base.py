aa3762e31bfb1d90f81cec3d8ce22d6c1fceac5e
David McGowan <dmcgweb101@gmail.com>
2016-03-14 19:57:58 +0000

    change debug

--- photozero/settings/base.py
+++ photozero/settings/base.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g(p^^8-_gl8zq5wxn(wi0($#aiuu0&ol+d9+j$wiq_xef4%9i+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
